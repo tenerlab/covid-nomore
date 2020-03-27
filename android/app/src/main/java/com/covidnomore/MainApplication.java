ab88d568c7b8d5392d5ec660ef5c64fb97f3fe64
Denis <dionis.serviciu@gmail.com>
2020-03-27 14:01:18 +0200

    chore: add CircularProgress dependency

--- android/app/src/main/java/com/covidnomore/MainApplication.java
+++ android/app/src/main/java/com/covidnomore/MainApplication.java
@@ -3,6 +3,7 @@ package com.covidnomore;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new RNDeviceInfo(),
             new AsyncStoragePackage(),
             new VectorIconsPackage(),
