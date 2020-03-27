6457cce967c761794a377df31f0c9aedaf7a1f68
Denis <dionis.serviciu@gmail.com>
2020-03-27 16:13:13 +0200

    feat: add button component

---
+++ src/screens/RiskScreen/ActionButton.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 4,
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+
+  basic: {
+    backgroundColor: '#8ACBE940',
+  },
+
+  positive: {
+    backgroundColor: '#FF5031',
+    marginTop: 15,
+  },
+
+  negative: {
+    backgroundColor: '#63F70D',
+    marginTop: 15,
+  },
+});
+
+const ActionButton = ({ variant = 'basic', text, onPress }) => {
+  const variantStyles = styles[variant] || styles.basic;
+
+  return (
+    <TouchableOpacity style={[styles.button, variantStyles]} onPress={onPress}>
+      <Text style={styles.buttonText}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default ActionButton;
