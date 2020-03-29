2ae15388438909339a118dde2b3e942c31431296
maxq13 <maxq13.white@gmail.com>
2020-03-29 20:43:03 +0300

    feat: update ProfileScreen (form and styles)

--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { StatusBar, Text, View } from 'react-native';
+import { dotPathOr } from 'ramda-extension';
+import moment from 'moment';
 import LinearGradient from 'react-native-linear-gradient';
 import { AppGlobals } from '@root/core/app-globals';
 import { styles } from './styles';
 
+/* ********************************** UTILS ********************************* */
+
+const preprocessUserDataForDisplay = user => {
+  let data = {};
+  data.firstName = dotPathOr('', 'firstName', user);
+  data.lastName = dotPathOr('', 'lastName', user);
+  data.fullName = `${data.firstName} ${data.lastName}`.trim();
+  data.sex = dotPathOr(null, 'sex', user);
+  data.sexToDisplay = '-';
+
+  if (data.sex == 'male' || data.sex === 1) data.sexToDisplay = 'Male';
+  if (data.sex == 'female' || data.sex === 0) data.sexToDisplay = 'Feale';
+
+  data.birthday = dotPathOr('', 'birthday', user);
+  data.birthdayToDisplay = data.birthday
+    ? moment(data.birthday, 'DD-MM-YYYY').format('MMMM D, YYYY')
+    : '-';
+
+  return data;
+};
+
+/* ********************************** MAIN ********************************** */
+
 // eslint-disable-next-line
 export const ProfileScreen = ({ navigation }) => {
   AppGlobals.acceptUpdates();
+
+  const currentUser = AppGlobals.getCurrentUser();
+  const userData = preprocessUserDataForDisplay(currentUser);
 
   return (
     <LinearGradient
@@ -14,13 +42,41 @@ export const ProfileScreen = ({ navigation }) => {
       colors={['#03A1E9', '#0183D3', '#0063BD']}
     >
       <StatusBar barStyle="light-content" backgroundColor="#03A1E9" />
-      <View>
-        <Text>Profile Screen</Text>
-      </View>
-      <View>
-        <Text style={styles.todoText}>
-          Current user: {JSON.stringify(AppGlobals.getCurrentUser())}
-        </Text>
+      <View style={styles.contentWrap}>
+        <View style={styles.profileSection}>
+          <View style={styles.profileData}>
+            <View style={styles.profileField}>
+              <Text style={styles.profileName}>{userData.fullName}</Text>
+            </View>
+            <View style={styles.profileField}>
+              <Text style={styles.fieldLabel}>Date of Birth:</Text>
+              <Text style={styles.fieldValue}>
+                {userData.birthdayToDisplay}
+              </Text>
+            </View>
+            <View style={styles.profileField}>
+              <Text style={styles.fieldLabel}>Sex:</Text>
+              <Text style={styles.fieldValue}>{userData.sexToDisplay}</Text>
+            </View>
+            <View style={styles.profileField}>
+              <Text style={styles.fieldLabel}>
+                Pre-Existing Health Conditions:
+              </Text>
+              <Text style={styles.fieldValue}>-</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.sharingSection}>
+          <View>
+            <Text>- Share symptom report</Text>
+          </View>
+          <View>
+            <Text>- Share pre-existing health conditions</Text>
+          </View>
+        </View>
+        <View style={styles.notificationsSection}>
+          <Text style={styles.notificationsSectionTitle}>Notifications</Text>
+        </View>
       </View>
     </LinearGradient>
   );
--- src/screens/ProfileScreen/styles.js
+++ src/screens/ProfileScreen/styles.js
@@ -6,9 +6,35 @@ export const styles = StyleSheet.create({
     flexDirection: 'column',
     backgroundColor: '#03A1E9',
     padding: 0,
-    alignItems: 'center',
     justifyContent: 'center',
     alignContent: 'center',
     position: 'relative',
+  },
+  contentWrap: {
+    margin: 30,
+    padding: 18,
+    minHeight: '80%',
+    backgroundColor: '#ffffff',
+  },
+  profileField: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginBottom: 5,
+  },
+  fieldLabel: {
+    marginRight: 5,
+  },
+  profileName: {
+    width: '100%',
+    textAlign: 'center',
+    marginBottom: 10,
+    fontSize: 20,
+  },
+
+  notificationsSection: {
+    marginTop: 30,
+  },
+  notificationsSectionTitle: {
+    fontSize: 16,
   },
 });
