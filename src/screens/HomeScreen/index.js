7cefc4be7dfaa4fa1b13899b92ebdba6aafe9484
maxq13 <maxq13.white@gmail.com>
2020-04-02 19:25:31 +0300

    feat: add header to HomeScreen

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import { StatusBar, Text, View } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
-// import { useTranslate } from '@root/hooks';
+import { ImageBackground, StatusBar, Text, View } from 'react-native';
+import { useTranslate } from '@root/hooks';
 import { styles } from './styles';
+
+const screenBgImg = require('@root/images/bg.png');
 
 // eslint-disable-next-line
 export const HomeScreen = ({ navigation }) => {
-  // const t = useTranslate();
-  // you can use t() as follows:
-  // t('common', 'Some text')
+  const t = useTranslate(); // eslint-disable-line no-unused-vars
+
+  // TODO: update dictionary.js and implement translation
+  // translation usage example: t('slides', 'some text')
 
   return (
-    <LinearGradient
-      style={styles.container}
-      colors={['#03A1E9', '#0183D3', '#0063BD']}
-    >
-      <StatusBar barStyle="light-content" backgroundColor="#03A1E9" />
-      <View>
-        <Text>Home Screen</Text>
-      </View>
-    </LinearGradient>
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="#20C1FF" />
+      <ImageBackground style={styles.screenBg} source={screenBgImg}>
+        <View style={styles.screenPage}>
+          <View style={styles.screenHeader}>
+            <Text style={styles.titleTexts}>
+              <Text style={styles.titleTextCovid}>COVID-19</Text>
+              <Text style={styles.titleTextNoMore}>no more</Text>
+            </Text>
+            <Text style={styles.slogan}>contribue la oprirea răspândirii</Text>
+            <View style={styles.scoreBoard}>
+              <View style={styles.scoreBlock}>
+                <View>
+                  <Text style={styles.scoreLabel}>total</Text>
+                </View>
+                <Text>
+                  <Text style={styles.scoreValue}>10</Text>
+                  <Text style={styles.scoreUnit}> pct</Text>
+                </Text>
+              </View>
+              <View style={styles.scoreBlock}>
+                <View>
+                  <Text style={styles.scoreLabel}>astâzi</Text>
+                </View>
+                <Text>
+                  <Text style={styles.scoreValue}>0 / 20</Text>
+                  <Text style={styles.scoreUnit}> pct</Text>
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.screenBody}>
+            <Text />
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
   );
 };
